60ec3584324e5adaa6bf70a0649e759f5d229934
ArseniyIT <113826000+ArseniyIT@users.noreply.github.com>
2022-09-18 15:56:45 +0700

    Первый урок в мовави на python 18 сентября

---
+++ main.py
@@ -0,0 +1,47 @@
+# print ("Hello World")
+# message1 = input("Сообщение 1:")
+# print ("Bye world")
+# message2 = input("Сообщение 2:")
+# x = 10
+# print(type(x))
+# a = 10.7
+# print(type(a))
+# b = '15'
+# print(type(b)
+# name = input("Имя :")
+# age = input("Возраст :")
+# time = input("Время :")
+#
+# print("Меня зовут", name,"Мне лет", age,"vremya", time)
+#
+# a = int(input("a:"))
+# b = int(input("b:"))
+#
+# s = a * b
+# p = (a + b) * 2
+#
+# print("S -->", s)
+# print("P -->", p)
+#
+# a = input("a:")
+# b = input("b:")
+#
+# c = a
+# a = b
+# b = c
+#
+# print("a = ", a)
+# print("b = ", b)
+#
+# a = int(input("a:"))
+# b = int(input("b:"))
+#
+# sr = (a+b)/2
+# print(" Среднее арефмитическое", sr)
+#
+# h = int(input("h:"))
+# m = int(input("m:"))
+# s = int(input("s:"))
+#
+# x = (h*60*60) + (m*60) + s
+# print("С начала дня прошло -->", x, "секунд.")
