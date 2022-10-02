d84b1f020480ab4ac2c3dedafe6f745fc0635bcb
movavi_school <andrejcepurnoj3@gmail.com>
2022-10-02 16:21:35 +0700

    третий урок

---
+++ lesson3/my.python.py
@@ -0,0 +1,61 @@
+# x = ["A", "Б", "В", "Г", "Д", "Е"]
+# print("Strochka", x)
+# print("First element", x[0])
+# print("Last element", x[-1])
+# print("pervie 5 elementov", x[0:5:2])
+
+#строка - это неизменяемый тип данных
+# x = "Прювет"
+# x[2] = "u"
+
+# x = "я карта, я рюкзак, я антон"
+# print(x[-5:])
+# print(x[:7])
+#
+# x = input("Введи слово")
+# temp = x[-1]
+# print(temp)
+# index = x.index(temp)
+# print(index + 1)
+#
+# print(len[x])
+# path = "C:/Python3/python.exe"
+# # # print("имя файла:", path[-10:])
+# # # print("расширкение:", path[-3:])
+# # # print("имя каталога:", path[3:10])
+# # # print("полный путь к каталогу:", path[0:10])
+# #
+# # path2 = path.split("/")
+# # print(path2)
+# #
+# # print("имя файла:", path2[-1])
+# # print("расширкение:", path2[-1][-3:])
+# # print("имя каталога:", path2[1])
+# # print(f"полный путь к каталогу:, {path2[0]}/{path2[1]}")
+#
+# chapter_1 = input("glava 1:")
+# page_1 = input("stranica: ")
+#
+# chapter_2 = input("glava 2:")
+# page_2 = input("stranica: ")
+#
+# chapter_3 = input("glava 3:")
+# page_3 = input("stranica: ")
+#
+# print(chapter_1.ljust(15), page_1.rjust(15))
+# print(chapter_2.ljust(15), page_2.rjust(15))
+# print(chapter_3.ljust(15), page_3.rjust(15))
+#
+# x = "12'345'678"
+# 1
+# temp = x.split("'")
+# print(temp)
+# number = int(temp[0] + temp[1] + temp[2])
+# print(number)
+# 2
+# temp = x[0:2] + x[3:6] + x[-3:]
+# number = int(temp)
+# print(temp)
+# 3
+# temp = x.replace("'", "")
+# print(temp)
