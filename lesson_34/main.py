34ef4b62b367b3709bc483d2d9309f55bdacec7d
movavi_school <misshell.helling@gmail.com>
2023-05-28 16:24:07 +0700

    28.05.23

---
+++ lesson_34/main.py
@@ -0,0 +1,37 @@
+# slovar = {"key1": "value1",
+#           "key2": "value2",
+#           "key3": "value3",
+#           "key4": "value4",
+#           "key5": "value5"}
+# # baran = list(slovar.keys())
+# # osel = list(slovar.values())
+# # print(baran, osel)
+# for i in slovar:
+#     print(slovar.keys(), slovar.values())
+
+
+
+# a = input()
+# count11 = 0
+# x = ["Никита", "Екатерина", "Андрей", "Андрей", "Анастасия", "Арсалан",
+#      "Наталья", "Тимур", "Григорий", "Евгений", "Анастасия",  "Екатерина",
+#      "Андрей", "Евгения", "Анастасия", "Герман", "Тимур", "Григорий",
+#      "Арсалан", "Ярослава", "Есения", "Даниил", "Данил", "Андрей", "Никита"]
+# b = x.count(a)
+# print(b)
+# for i in x:
+#      if a == i:
+#           count += 1
+# print(count)
+
+# import math
+# a = float(input())
+# b = math.ceil(a)
+# print(b)
+
+# a = float(input())
+# b = str
+# for i in a:
+#      if a[i] > a[i]:
+#           print("sdfsdf")
+
